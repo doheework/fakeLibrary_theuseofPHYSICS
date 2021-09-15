3f5c9e1a98528f7e1bd89882ed009e5723d81813
Your Namedo <you@example.com>
2021-09-15 10:39:33 +0100

    1.1.1

---
+++ sketch.js
@@ -0,0 +1,218 @@
+$(document).ready(function(){
+    $(".울버린").mouseover(function(){
+        var image = $(".울버린i");
+    $(".울버린i").show();
+    $(".울버린i").stop().animate({top:window.innerHeight-image.innerHeight()+200});
+    });
+    $(".울버린").mouseout(function(){
+    $(".울버린i").stop().animate({top:"100%"});
+    
+    });
+    });
+
+$(document).ready(function(){
+    $(".수업").mouseover(function(){
+        var image = $(".수업i");
+    $(".수업i").show();
+    $(".수업i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2})
+    });
+    $(".수업").mouseout(function(){
+    $(".수업i").stop().animate({top:"100%"})
+    });
+    });
+
+ $(document).ready(function(){
+    $(".확대").mouseover(function(){
+        var image = $(".확대i");
+    $(".확대i").show();
+    $(".확대i").stop().animate({left:window.innerWidth-image.innerWidth()})
+    });
+    $(".확대").mouseout(function(){
+    $(".확대i").stop().animate({left:"100%"})
+    });
+    });   
+
+ $(document).ready(function(){
+    $(".그림단어").mouseover(function(){
+        var image = $(".그림단어i");
+    $(".그림단어i").show();
+    $(".그림단어i").stop().animate({top:window.innerHeight-image.innerHeight()})
+    });
+    $(".그림단어").mouseout(function(){
+    $(".그림단어i").stop().animate({top:"100%"})
+    });
+    });   
+
+$(document).ready(function(){
+    $(".시즌").mouseover(function(){
+    $(".시즌i").show();
+    $(".시즌i").stop().animate({top:0})
+    });
+    $(".시즌").mouseout(function(){
+    $(".시즌i").stop().animate({top:"100%"})
+    });
+    });
+
+ $(document).ready(function(){
+    $(".등장").mouseover(function(){
+        var image = $(".등장i");
+    $(".등장i").show();
+    $(".등장i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2})
+    });
+    $(".등장").mouseout(function(){
+    $(".등장i").stop().animate({top:"100%"})
+    });
+    });   
+
+ $(document).ready(function(){
+    $(".학생").mouseover(function(){
+        var image = $(".학생i");
+    $(".학생i").show();
+    $(".학생i").stop().animate({top:window.innerHeight-image.innerHeight()})
+    });
+    $(".학생").mouseout(function(){
+    $(".학생i").stop().animate({top:"100%"})
+    });
+    });   
+
+ $(document).ready(function(){
+    $(".주빌리").mouseover(function(){
+        var image = $(".주빌리i");
+    $(".주빌리i").show();
+    $(".주빌리i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2})
+    });
+    $(".주빌리").mouseout(function(){
+    $(".주빌리i").stop().animate({top:"100%"})
+    });
+    });   
+
+ $(document).ready(function(){
+    $(".플라즈모이드").mouseover(function(){
+        var image = $(".플라즈모이드i");
+    $(".플라즈모이드i").show();
+    $(".플라즈모이드i").stop().animate({top:window.innerHeight-image.innerHeight()})
+    });
+    $(".플라즈모이드").mouseout(function(){
+    $(".플라즈모이드i").stop().animate({top:"100%"})
+    });
+    });   
+
+ $(document).ready(function(){
+    $(".핵융합").mouseover(function(){
+        var image = $(".핵융합i");
+    $(".핵융합i").show();
+    $(".핵융합i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2})
+    });
+    $(".핵융합").mouseout(function(){
+    $(".핵융합i").stop().animate({top:"100%"})
+    });
+    });   
+
+ $(document).ready(function(){
+    $(".손").mouseover(function(){
+        var image = $(".손i");
+    $(".손i").show();
+    $(".손i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2})
+    });
+    $(".손").mouseout(function(){
+    $(".손i").stop().animate({top:"100%"})
+    });
+    });   
+
+ $(document).ready(function(){
+    $(".불꽃놀이").mouseover(function(){
+        var image = $(".불꽃놀이i");
+    $(".불꽃놀이i").show();
+    $(".불꽃놀이i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2})
+    });
+    $(".불꽃놀이").mouseout(function(){
+    $(".불꽃놀이i").stop().animate({top:"100%"})
+    });
+    });   
+
+ $(document).ready(function(){
+    $(".게임기").mouseover(function(){
+        var image = $(".게임기i");
+    $(".게임기i").show();
+    $(".게임기i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2})
+    });
+    $(".게임기").mouseout(function(){
+    $(".게임기i").stop().animate({top:"100%"})
+    });
+    });   
+
+$(document).ready(function(){
+    $(".파이로").mouseover(function(){
+        var image = $(".파이로i");
+    $(".파이로i").show();
+    $(".파이로i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2})
+    });
+    $(".파이로").mouseout(function(){
+    $(".파이로i").stop().animate({top:"100%"})
+    });
+    });   
+
+$(document).ready(function(){
+    $(".라이터").mouseover(function(){
+        var image = $(".라이터i");
+    $(".라이터i").show();
+    $(".라이터i").stop().animate({left:window.innerWidth-image.innerWidth()})
+    });
+    $(".라이터").mouseout(function(){
+    $(".라이터i").stop().animate({left:"100%"})
+    });
+    });   
+
+$(document).ready(function(){
+    $(".압전기").mouseover(function(){
+        var image = $(".압전기i");
+    $(".압전기i").show();
+    $(".압전기i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2})
+    });
+    $(".압전기").mouseout(function(){
+    $(".압전기i").stop().animate({top:"100%"})
+    });
+    });   
+
+$(document).ready(function(){
+    $(".바비").mouseover(function(){
+        var image = $(".바비i");
+    $(".바비i").show();
+    $(".바비i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2})
+    });
+    $(".바비").mouseout(function(){
+    $(".바비i").stop().animate({top:"100%"})
+    });
+    });   
+
+$(document).ready(function(){
+    $(".얼음").mouseover(function(){
+        var image = $(".얼음i");
+    $(".얼음i").show();
+    $(".얼음i").stop().animate({top:window.innerHeight-image.innerHeight()})
+    });
+    $(".얼음").mouseout(function(){
+    $(".얼음i").stop().animate({top:"100%"})
+    });
+    });   
+
+$(document).ready(function(){
+    $(".장면").mouseover(function(){
+        var image = $(".장면i");
+    $(".장면i").show();
+    $(".장면i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2})
+    });
+    $(".장면").mouseout(function(){
+    $(".장면i").stop().animate({top:"100%"})
+    });
+    });   
+
+$(document).ready(function(){
+    $(".성적표").mouseover(function(){
+    $(".성적표i").show();
+    $(".성적표i").stop().animate({top:0})
+    });
+    $(".성적표").mouseout(function(){
+    $(".성적표i").stop().animate({top:"100%"})
+    });
+    });   
